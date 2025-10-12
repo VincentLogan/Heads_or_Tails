72d4cd104652b172afc5aa0e923c381ca49eb134
ysf0701 <3231446573@qq.com>
2025-10-12 16:27:25 +0800

    卡牌提示和抽牌弃牌
    目前缺少前面的代码，程序无法运行，以及卡牌颜色未添加

--- heads-or-tails/global/events.gd
+++ heads-or-tails/global/events.gd
@@ -1,5 +1,16 @@
 extends Node
 
 #Card_related events
+#这里应该还有几个signal，应该是第5集的
+#signal card_drag_started(card_ui: CardUI)
+#signal card_drag_ended(card_ui: CardUI)
+#signal card_played(card: Card)
 signal card_aim_started(card_ui: CardUI)
 signal card_aim_ended(card_ui: CardUI)
+signal card_tooltip_requested(card: Card)
+signal tooltip_hide_requested
+
+# Player-related events
+signal player_hand_drawn
+signal player_hand_discarded
+signal player_turn_ended
--- heads-or-tails/scenes/card_ui/card_states/card_relessed_state.gd
+++ heads-or-tails/scenes/card_ui/card_states/card_relessed_state.gd
@@ -4,13 +4,14 @@ var played: bool
 
 func enter() -> void:
 	card_ui.color.color = Color.DARK_VIOLET
-	card_ui.state.text = "RELEASED"
+	card_ui.state.text = "RELEASED"#这里好像要删掉
 
 	played = false
 	
 	if not card_ui.targets.is_empty():
+		Events.tooltip_hide_requested.emit()
 		played = true
-		print("play card for target(s)", card_ui.targets)
+		print("play card for target(s)", card_ui.targets)#这里好像要删掉，改成card_ui.play()
 		
 func  on_input(_event: InputEvent) -> void:
 	if played:
