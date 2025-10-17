11e0bbecf002bf981196c2980f1f7e5e7e986f2d
VincentLogan <1771504139@qq.com>
2025-10-17 21:54:44 +0800

    卡牌创建
    缺少代码

---
+++ heads-or-tails/characters/warrior/cards/lucky_attack.gd
@@ -0,0 +1,7 @@
+extends Card
+
+@export var optional_sound: AudioStream
+
+func apply_effects(_targets: Array[Node]) -> void:
+	print("My awesome card has been played!")
+	print("Targets: %s" % _targets)
