9d973ac42aa69fbca1734dca4fbe3090956f5911
ordll <466220187@qq.com>
2025-10-19 22:16:22 +0800

    幸运一击再次改动

--- heads-or-tails/characters/warrior/cards/lucky_attack.gd
+++ heads-or-tails/characters/warrior/cards/lucky_attack.gd
@@ -8,11 +8,15 @@ func apply_effects(targets: Array[Node], char_stats: CharacterStats) -> void:
 	var luck_coins = char_stats.luck_coins
 	luck_effect.amount = coin_gain
 	if luck_coins <= 50:
-		damage_effect.amount = 3
+		damage_effect.amount = 7
+		if luck_coins < 20:
+			char_stats.luck_coins = 0
+		else:
+			char_stats.luck_coins -= 20
 	else:
 		var damage_effect_amount = luck_coins - 50
-		damage_effect.amount = damage_effect_amount / 2
-		char_stats.luck_coins = 50
+		damage_effect.amount = 7 + damage_effect_amount / 2
+		char_stats.luck_coins -= 20
 		GlobalSignals.card_effect_triggered.emit("so_lucky")
 		
 	damage_effect.sound = sound
