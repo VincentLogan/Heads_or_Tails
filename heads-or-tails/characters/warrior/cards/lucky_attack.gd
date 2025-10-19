3a0809efdfe471c7b387d797e8b97c10be1c2363
ordll <466220187@qq.com>
2025-10-19 22:01:49 +0800

    修改了幸运一击的攻击倍率，修复了其不扣幸运值的bug

--- heads-or-tails/characters/warrior/cards/lucky_attack.gd
+++ heads-or-tails/characters/warrior/cards/lucky_attack.gd
@@ -8,11 +8,11 @@ func apply_effects(targets: Array[Node], char_stats: CharacterStats) -> void:
 	var luck_coins = char_stats.luck_coins
 	luck_effect.amount = coin_gain
 	if luck_coins <= 50:
-		damage_effect.amount = 7
+		damage_effect.amount = 3
 	else:
 		var damage_effect_amount = luck_coins - 50
-		damage_effect.amount = damage_effect_amount
-		luck_coins = 50
+		damage_effect.amount = damage_effect_amount / 2
+		char_stats.luck_coins = 50
 		GlobalSignals.card_effect_triggered.emit("so_lucky")
 		
 	damage_effect.sound = sound
